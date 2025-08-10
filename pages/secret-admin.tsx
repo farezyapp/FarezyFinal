55dc2055570d422810d81518a0f1b31418600742
farezyapp <cian@farezy.co.uk>
2025-08-10 11:08:04 +0100

    Delete pages directory

--- pages/secret-admin.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AdminDashboard from './admin-dashboard';
-
-// Hidden admin route - only accessible by direct URL
-const SecretAdmin: React.FC = () => {
-  return <AdminDashboard />;
-};
-
-export default SecretAdmin;
